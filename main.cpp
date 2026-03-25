b6d04128f81d707536ec7ba09cdc7d876aec41a2
hanryu98 <hanryu98@users.noreply.github.com>
2026-03-25 10:59:31 +0900

    DynamicArray
    동적배열 2교시

---
+++ main.cpp
@@ -0,0 +1,26 @@
+//Standard Template Library
+# include <iostream>
+# include <vector>
+# include "DynamicArray.h"
+
+
+using namespace std;
+
+int main()
+{
+	vector<int> IntArray;
+
+	DynamicArray OurArray;
+
+	OurArray.PushBack(1);
+	OurArray.PushBack(2);
+	OurArray.PushBack(3);
+	OurArray.PushBack(4);
+	
+	for (int i = 0; i < OurArray.GetSize(); i++)
+	{
+		cout<<OurArray.Data[i] << endl;
+	}
+
+	return 0;
+}
