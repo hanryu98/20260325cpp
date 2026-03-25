d459fa824f5f35b99a0cda6d2698574299b46e27
hanryu98 <hanryu98@users.noreply.github.com>
2026-03-25 14:12:06 +0900

    Dynamic Array
    Overloading

--- DynamicArray.h
+++ DynamicArray.h
@@ -5,11 +5,12 @@
 class DynamicArray
 {
 public:
-	DynamicArray()
+	
+	DynamicArray(int InitialCapacity = 10)
 	{
 		Size = 0;
-		Data = nullptr;
-		//Data = new int[Size];
+		Capacity = InitialCapacity;
+		Data = new int[InitialCapacity];
 	}
 
 	~DynamicArray()
@@ -18,53 +19,76 @@ public:
 		Data = nullptr;
 	}
 
+       
 	void PushBack(int InValue)
 	{
 		Size++;
-		// 1. 늘어난 영역만큼 새로 할당
-		int* NewData = new int[Size];
 
-		//2. 원본 복제
-		for (int i = 0; i < Size - 1; i++)
+		int* NewData = nullptr;
+		
+		if (Size > Capacity)
 		{
-			NewData[i] = Data[i];
+			Capacity = Capacity * 2;
+			NewData = new int[Capacity];
+			
+			for (int i = 0; i < Size - 1; ++i)
+			{
+				NewData[i] = Data[i];
+			}
 
+			
+			delete[] Data;
+
+			
+			Data = NewData;
 		}
+		
+		Data[Size - 1] = InValue;
 
-		//3. 새로 들어온 값을 넣는다
-		NewData[Size - 1] = InValue;
-
-		//4. 원래 있던걸 지운다
-		delete[] Data;
-
-		//5. 원래 데이터의 포인터에 새 데이터 포인터 연결
-		Data = NewData;
 
 	}
-	
-	//함수 뒤에 const read only 함수임
+
 	inline size_t GetSize() const
 	{
-
 		return Size;
 	}
 
-	int& operator [] (int Index) const
+	inline size_t GetCapacity() const
 	{
+		return Capacity;
+	}
+
+	const int& operator[](int Index) const
+	{
+		if (Index < 0 || Index > Size)
+		{
+			return 0;
+		}
+
 		return Data[Index];
 	}
 
-//protected:
+	void RemoveAt(int RemoveIndex)
+	{
+		if (RemoveIndex >= Size || RemoveIndex < 0)
+		{
+			return;
+		}
+
+		for (int i = RemoveIndex; i < Size - 1; ++i)
+		{
+			Data[i] = Data[i + 1];
+		}
+
+		Size--;
+	}
+
+protected:
 	int* Data;
 	size_t Size = 0;
+	size_t Capacity = 1;
 };
 
-class FVector
-{
-	FVector operator+(const FVector& RHS)
-	{
 
-	}
-};
 
-#endif
+#endif 
--- main.cpp
+++ main.cpp
@@ -1,26 +1,77 @@
-//Standard Template Library
-# include <iostream>
-# include <vector>
-# include "DynamicArray.h"
-
+#include <iostream>
+#include <vector>
+#include "DynamicArray.h"
 
 using namespace std;
 
+void Test(int A = 2, int B = 1, int C = 0)
+{
+	cout << A << endl;
+	cout << B << endl;
+	cout << C << endl;
+}
+
 int main()
 {
-	vector<int> IntArray;
+	Test(1, 2, 3);
 
-	DynamicArray OurArray;
+	return 0;
 
-	OurArray.PushBack(1);
-	OurArray.PushBack(2);
-	OurArray.PushBack(3);
-	OurArray.PushBack(4);
-	
-	for (int i = 0; i < OurArray.GetSize(); i++)
+	DynamicArray D(4);
+
+	for (int i = 0; i < 8; ++i)
 	{
-		cout<<OurArray.Data[i] << endl;
+		cout << D.GetSize() << endl;
+		cout << D.GetCapacity() << endl;
+		cout << "----------------------------" << endl;
+
+		D.PushBack(i + 1);
 	}
+
+	//[1][2][3][4][5][6][7][8]
+	D.RemoveAt(3);
+
+	cout << D.GetSize() << endl;
+	cout << D.GetCapacity() << endl;
+	cout << "----------------------------" << endl;
+	for (int i = 0; i < D.GetSize(); ++i)
+	{
+		cout << D[i] << ", ";
+	}
+	//[1][2][3][5][6][7][8]
+
+	cout << endl;
+
+	D.RemoveAt(5);
+
+	cout << D.GetSize() << endl;
+	cout << D.GetCapacity() << endl;
+	cout << "----------------------------" << endl;
+
+
+	for (int i = 0; i < D.GetSize(); ++i)
+	{
+		cout << D[i] << ", ";
+	}
+	//[1][2][3][5][6][8]
+	cout << endl;
+
+	D.PushBack(9);
+
+	for (int i = 0; i < D.GetSize(); ++i)
+	{
+		cout << D[i] << ", ";
+	}
+	//[1][2][3][5][6][8][9]
+	cout << endl;
+
+	cout << D.GetSize() << endl;
+	cout << D.GetCapacity() << endl;
+	cout << "----------------------------" << endl;
+
+
+
+
 
 	return 0;
 }
